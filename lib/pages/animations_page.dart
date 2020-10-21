3019fa0f09a66c6ff78932022969c869439dac1e
julian <juliandanielfranco@gmail.com>
2020-10-20 21:35:05 -0500

    feat: improve animations

--- lib/pages/animations_page.dart
+++ lib/pages/animations_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as Math;
 
 class AnimationsPage extends StatelessWidget {
   @override
@@ -17,9 +18,7 @@ class _Rectagle extends StatelessWidget {
     return Container(
       width: 70,
       height: 70,
-      decoration: BoxDecoration(
-        color: Colors.red
-      ),
+      decoration: BoxDecoration(color: Colors.red),
     );
   }
 }
@@ -29,11 +28,66 @@ class AnimatedRectagle extends StatefulWidget {
   _AnimatedRectagleState createState() => _AnimatedRectagleState();
 }
 
-class _AnimatedRectagleState extends State<AnimatedRectagle> {
+class _AnimatedRectagleState extends State<AnimatedRectagle>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> rotation;
+  Animation<double> opacity;
+  Animation<double> moveToRight;
+  Animation<double> scale;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    controller = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 4000));
+    rotation = Tween(begin: 0.0, end: 2.0 * Math.pi)
+        .animate(CurvedAnimation(parent: controller, curve: Curves.easeOut));
+
+    opacity = Tween(begin: 0.1, end: 1.0).animate((CurvedAnimation(
+        parent: controller, curve: Interval(0, 0.25, curve: Curves.easeOut))));
+
+    moveToRight = Tween(begin: 0.0, end: 200.0)
+        .animate(CurvedAnimation(parent: controller, curve: Curves.easeOut));
+
+    scale = Tween(begin: 0.0, end: 2.0)
+        .animate(CurvedAnimation(parent: controller, curve: Curves.easeOut));
+
+    controller.addListener(() {
+      print('Status ${controller.status}');
+
+      if (controller.status == AnimationStatus.completed) {
+        controller.reverse();
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return _Rectagle();
+    controller.forward();
+    return AnimatedBuilder(
+      animation: controller,
+      child: _Rectagle(),
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(moveToRight.value, 0),
+          child: Transform.rotate(
+              angle: rotation.value,
+              child: Opacity(
+                opacity: opacity.value,
+                child: Transform.scale(scale: scale.value, child: child),
+              )),
+        );
+      },
+    );
   }
 }
-
-
