fd7b666d0795ae5c57cfa36a5af3638ff5cdf69a
julian <juliandanielfranco@gmail.com>
2020-10-23 14:15:30 -0500

    feat: add slideshow provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:custom_painter/pages/animations_page.dart';
 import 'package:custom_painter/labs/circular_progress_page.dart';
 import 'package:custom_painter/pages/circular_graphics_page.dart';
 import 'package:custom_painter/pages/header_page.dart';
+import 'package:custom_painter/pages/slideshow_page.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: CircularGraphics(),
+    home: SlideShowPage(),
   ));
 }
 
